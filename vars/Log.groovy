8ae83052efe09a86df8572dd27e541178571cdc1
Mukul Sharma <mushar1l@in.ibm.com>
2019-05-28 16:23:54 +0530

    added the library files

---
+++ vars/Log.groovy
@@ -0,0 +1,14 @@
+def call(String name = 'LogGroovy') {
+   echo "Hello, ${name}."
+}
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+
+
+
